edc789c24c91e3b82d64eae50778669ca4bea0f1
Shailender sharma <62423574+Shailender9207@users.noreply.github.com>
2021-10-10 22:54:14 +0530

    Adding comment
    add comments

--- guess game.py
+++ guess game.py
@@ -6,11 +6,12 @@ out_of_guess = False
 
 while guess !=code_word and not  out_of_guess :
     if guess_count < guess_limit :
-        guess=input("enter your guess : ")
+        guess=input("enter your guess : ") // enter INPUT
         guess_count += 1
     else :
         out_of_guess = True
 if out_of_guess:
-    print("out of guess YOU LOSE !")
+    print("out of guess YOU LOSE !") //output
 else:
-    print("Congorats YOU WIN !")
+    print("Congorats YOU WIN !") //output
+    
